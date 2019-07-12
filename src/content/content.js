b52a74cec2e8f89fa4884b2c1b7d9943b438a86d
necoco <neconecocon@gmail.com>
2019-07-12 19:46:11 +0900

    入力フォームにフォーカスを与えてからペーストするように変更

--- src/content/content.js
+++ src/content/content.js
@@ -1,6 +1,7 @@
 const HASHTAG = "#100DaysOfCode";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    document.getElementById('tweet-box-home-timeline').focus();
     chrome.runtime.sendMessage({ method: 'getDays' }, function (response) {
         document.activeElement.innerText += `Day${response.data} ${HASHTAG}`;
     });
