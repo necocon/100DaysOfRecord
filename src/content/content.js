d3abb90f59843b00992506fed306fc4130b5f6fd
kuroppe <kuroppems@gmail.com>
2019-07-13 20:55:37 +0900

    コードのフォーマットとコメントを修正

--- src/content/content.js
+++ src/content/content.js
@@ -1,6 +1,7 @@
 const MIN_COUNT = 1;
 const templateText = (text) => `Day${text} #100DaysOfCode`;
  
+// backgrounから通知を受け取る
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     // 何も選択されていない場合はbodyかnullが返ってくる。
     const activeElement = document.activeElement;
