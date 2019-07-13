c09ac0bde3526530e2542bfffd94d92f5ab04b34
kuroppe <kuroppems@gmail.com>
2019-07-14 00:39:05 +0900

    拡張機能のインストール時にのみContextMenuを新規作成するようにした

--- src/background/background.js
+++ src/background/background.js
@@ -1,13 +1,16 @@
 'use strict';
-chrome.contextMenus.create({
-	'title': '日数とハッシュタグを貼り付け',
-	'type': 'normal',
-    'contexts': ['all'],
-    'id': 'parent'
+// 拡張機能のインストール時に実行される
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        'title': '日数とハッシュタグを貼り付け',
+        'type': 'normal',
+        'contexts': ['all'],
+        'id': 'parent'
+    });
 });
 
 // ContextMenuaから「日数とハッシュタグを貼り付け」をクリックしたときに第2引数に渡した関数が実行される。
-chrome.contextMenus.onClicked.addListener(function (info, tab){
+chrome.contextMenus.onClicked.addListener((info, tab) => {
     // backgroundからcontentやpopupにメッセージを通知する。
     chrome.tabs.sendMessage(tab.id, 'action');
 });
